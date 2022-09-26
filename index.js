82ded1368c6a69fe053869c67d80aa1663bb96fe
239yash <239yash@gmail.com>
2022-09-26 23:45:19 +0530

    test: redis connection stability tests

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const redis = require("redis");
 
+const client = redis.createClient({
+    socket: {
+        host: "localhost",
+        port: 6379,
+    },
+    password: "changeit",
+});
+
+client.on("error", (err) => {
+    console.log("Error On Connection" + err);
+});
+client.connect();
 app.get("/get-redis-key", (req, res) => {
-  res.send("Hello World!");
+    console.log("first");
+    client.set("foo", "bar", (err, reply) => {
+        if (err) throw err;
+        console.log(reply);
+
+        client.get("foo", (err, reply) => {
+            if (err) throw err;
+            console.log(reply);
+        });
+    });
+    console.log("second")
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Redis-Snappy app listening on port ${port}`);
 });
