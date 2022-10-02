3db31fcbac02818908afcea563d38de3ef964af4
239yash <239yash@gmail.com>
2022-10-02 20:00:30 +0530

    add: Blob testing for string size

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const redis = require("redis");
+const snappy = require("snappy");
+const testData = require("./testData");
+const Blob = require('node-blob');
 const app = express();
 const port = 3000;
-const redis = require("redis");
 
 const client = redis.createClient({
     socket: {
@@ -18,9 +21,18 @@ client.on("connect", () => {
     console.log("Redis client connected");
 })
 
+const testDataPromise = async () => {
+    try {
+        var buffer = new Blob([JSON.stringify(testData)]);
+        console.log(buffer.size);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
 app.get("/get-redis-key", async (req, res) => {
     let cachedData = await client.get("foo");
-    console.log("Redis Data is - " + cachedData);
+    testDataPromise();
     res.status(200).send(cachedData);
 });
 
